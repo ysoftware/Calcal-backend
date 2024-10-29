eed8d72796250092d3cf43047222f4d4fe86687e
Yaroslav Erohin <ysoftware@yandex.ru>
2024-10-29 18:14:34 +0100

    swift works on linux

--- src/Main.swift
+++ src/Main.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 @main struct App {
     static func main() {
--- src/Model.swift
+++ src/Model.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 final class Model: @unchecked Sendable {
     
