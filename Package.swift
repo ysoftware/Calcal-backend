f3069abdabbd85bf96a9ecef5cca8a5d22f20e8e
Yaroslav Erohin <ysoftware@yandex.ru>
2024-10-29 19:11:12 +0100

    get getting got

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "Calcal-backend",
-    platforms: [
-        .macOS(.v12)
+    platforms: [ .macOS(.v12) ],
+    dependencies: [
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0")
     ],
     targets: [
-        .executableTarget(name: "Calcal-backend")
+        .executableTarget(name: "Calcal-backend", dependencies: [.product(name: "Hummingbird", package: "hummingbird")])
     ]
 )
--- src/Main.swift
+++ src/Main.swift
@@ -1,19 +1,32 @@
 import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
+import Hummingbird
 
 @main struct App {
-    static func main() {
-        let parser = Parser(text: """
-                            Date: 1 April, 2024
+    static func main() async throws {
+        let router = Router()
 
-                            Breakfast
-                            - Cappuccino, 1, 45 kcal
+        router.get("main.php") { request, _ -> String in
+            print("got get: \(request)")
 
-                            Total: 45 kcal
-                            """)
-        let result = try! parser.parse()
-        print("\(result)")
+            let url = URL(fileURLWithPath: "data.txt")
+            do { 
+                let data = try Data(contentsOf: url)
+                let contents = String(data: data, encoding: .utf8)
+                return contents ?? "Error"
+            } catch {
+                return "\(error)"
+            }
+        }
+
+        router.post("main.php") { request, _ -> String in
+            print("got post: \(request)")
+            return ""  
+        }
+
+        let app = Application(
+            router: router,
+            configuration: .init(address: .hostname("127.0.0.1", port: 7070))
+        )
+        try await app.runService()
     }
 }
