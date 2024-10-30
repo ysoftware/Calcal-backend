c3b99cfbff595226a2b8a1302fa7ed3ecefc03a7
Iaroslav Erokhin <ysoftware@yandex.ru>
2024-10-30 13:09:19 +0100

    implement post

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "Calcal-backend",
-    platforms: [ .macOS(.v12) ],
+    platforms: [ .macOS(.v14) ],
     dependencies: [
-        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0")
+        .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.3.0"),
+        .package(url: "https://github.com/vapor/multipart-kit.git", from: "4.7.0"),
     ],
     targets: [
-        .executableTarget(name: "Calcal-backend", dependencies: [.product(name: "Hummingbird", package: "hummingbird")])
+        .executableTarget(name: "Calcal-backend", dependencies: [
+            .product(name: "Hummingbird", package: "hummingbird"),
+            .product(name: "MultipartKit", package: "multipart-kit"),
+        ])
     ]
 )
--- src/Main.swift
+++ src/Main.swift
@@ -1,15 +1,21 @@
 import Foundation
 import Hummingbird
+import MultipartKit
+
+struct PostBody: Decodable {
+    let file: String
+    let password: String
+}
 
 @main struct App {
     static func main() async throws {
         let router = Router()
 
         router.get("main.php") { request, _ -> String in
-            print("got get: \(request)")
+            print("got get")
 
-            let url = URL(fileURLWithPath: "data.txt")
-            do { 
+            do {
+                let url = URL(fileURLWithPath: "data.txt")
                 let data = try Data(contentsOf: url)
                 let contents = String(data: data, encoding: .utf8)
                 return contents ?? "Error"
@@ -18,14 +24,85 @@ import Hummingbird
             }
         }
 
-        router.post("main.php") { request, _ -> String in
-            print("got post: \(request)")
-            return ""  
+        router.post("main.php") { request, context -> Response in
+            guard let contentType = request.headers[.contentType],
+                  let mediaType = MediaType(from: contentType),
+                  let parameter = mediaType.parameter,
+                  parameter.name == "boundary"
+            else { return Response(status: .badRequest, headers: HTTPFields(), body: ResponseBody()) }
+
+            let postBody: PostBody
+            do {
+                let buffer = try await request.body.collect(upTo: context.maxUploadSize)
+                postBody = try FormDataDecoder().decode(PostBody.self, from: buffer, boundary: parameter.value)
+            } catch {
+                print("got post: bad request \(#line)")
+                return Response(status: .badRequest, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: "password.txt"))
+                guard let password = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+                    print("got post: server error \(#line)")
+                    return Response(status: .internalServerError, headers: HTTPFields(), body: ResponseBody())
+                }
+                
+                if postBody.password != password {
+                    print("got post: forbidden \(#line)")
+                    return Response(status: .forbidden, headers: HTTPFields(), body: ResponseBody())
+                }
+            } catch {
+                print("got post: server error \(#line)")
+                return Response(status: .internalServerError, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            var newData: [EntryEntity]
+            do {
+                newData = try Parser(text: postBody.file).parse()
+            } catch {
+                print("got post: bad request \(#line)")
+                return Response(status: .badRequest, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            var existingData: [EntryEntity] = []
+            do {
+                if let data = try? Data(contentsOf: URL(fileURLWithPath: "data.txt")), 
+                    let contents = String(data: data, encoding: .utf8) {
+                    existingData = try Parser(text: contents).parse()
+                }
+            } catch {
+                print("got post: server error \(#line)")
+                return Response(status: .internalServerError, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            for entry in newData {
+                if let foundIndex = existingData.firstIndex(where: { $0.date == entry.date }) {
+                    existingData[foundIndex] = entry
+                } else {
+                    existingData.append(entry)
+                }
+            }
+
+            guard let data = existingData.map(Mapper.map(entity:)).joined(separator: "\n\n").data(using: .utf8)
+            else { 
+                print("got post: server error \(#line)")
+                return Response(status: .internalServerError, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            do {
+                try data.write(to: URL(fileURLWithPath: "data.txt"))
+            } catch {
+                print("got post: server error \(#line)")
+                return Response(status: .internalServerError, headers: HTTPFields(), body: ResponseBody())
+            }
+
+            print("got post: ok")
+            return Response(status: .ok, headers: HTTPFields(), body: ResponseBody())
         }
 
         let app = Application(
             router: router,
-            configuration: .init(address: .hostname("127.0.0.1", port: 7070))
+            configuration: .init(address: .hostname("127.0.0.1", port: 8000))
         )
         try await app.runService()
     }
